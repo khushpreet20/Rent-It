55691134a5f26007a7dac9f361d41b4b671dea08
khushpreet20 <khushisandhu0502@gmail.com>
2024-08-31 00:25:24 +0530

    Changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,7 +8,6 @@ axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 console.log(import.meta.env.VITE_API_BASE_URL);
 axios.defaults.withCredentials = true;
 
-
 function App() {
   return (
     <>
