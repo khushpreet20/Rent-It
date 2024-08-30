a3c7ed37cb032a9ce579de960d23cf97d1d2c22b
khushpreet20 <khushisandhu0502@gmail.com>
2024-08-31 00:18:00 +0530

    Changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,7 +6,6 @@ import Footer from './components/Footer.jsx'
 import axios from 'axios';
 axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 console.log(import.meta.env.VITE_API_BASE_URL);
-
 axios.defaults.withCredentials = true;
 
 
